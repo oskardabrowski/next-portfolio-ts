dbe3a07b6e21d9ae6f9d9bf3f99e80847c8cf3c9
Oskar Dabrowski <oskar.dabrowski12890@wp.pl>
2023-05-16 00:21:18 +0200

    Adding images

---
+++ components/body/projects/ProjectsHeader.tsx
@@ -0,0 +1,194 @@
+import { NextPage } from "next";
+import React from "react";
+import styled from "styled-components";
+import Image from "next/image";
+import Particles from "react-particles";
+import { Engine } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+
+
+const ProjectsHeader:NextPage = () => {
+  const particlesInit = useCallback(async (engine: Engine) => {
+        await loadFull(engine);
+    }, []);
+    const particlesLoaded = useCallback(async (container: any) => {
+    }, []);
+  return (
+    <ProjectsHead>
+        <h1>Discover my projects<span className="colored">!</span></h1>
+        <section id="ParticlesBackground">
+           <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            className = "CanvasWrapper"
+
+            options={{
+                background: {
+                    color: "transparent",
+                },
+                fpsLimit: 60,
+                fullScreen: false,
+                interactivity: {
+                  detectsOn: "canvas",
+                  events: {
+                    onHover: {
+                      enable: true,
+                      mode: "bubble"
+                    },
+                    resize: true
+                  },
+                  modes: {
+                    bubble: {
+                      distance: 100,
+                      duration: 2,
+                      opacity: 1,
+                      size: 10
+                    }
+                  }
+                },
+                particles: {
+                  color: {
+                    value: "#0ff"
+                  },
+                  links: {
+                    blink: false,
+                    color: "transparent",
+                    consent: false,
+                    distance: 40,
+                    enable: true,
+                    opacity: 0.3,
+                    width: 0.5
+                  },
+                  move: {
+                    attract: {
+                      enable: false,
+                      rotate: {
+                        x: 600,
+                        y: 1200
+                      }
+                    },
+                    bounce: false,
+                    direction: "none",
+                    enable: true,
+                    outMode: "bounce",
+                    random: false,
+                    speed: 1,
+                    straight: false
+                  },
+                  decay: {
+                    enable: true,
+                    value: 0.05,
+                  },
+                  number: {
+                    density: {
+                      enable: false,
+                      area: 2000
+                    },
+                    limit: 0,
+                    value: 400
+                  },
+                  opacity: {
+                    animation: {
+                      enable: false,
+                      minimumValue: 0.05,
+                      speed: 2,
+                      sync: false
+                    },
+                    random: false,
+                    value: 0.5
+                  },
+                  shape: {
+                    type: "circle"
+                  },
+                  size: {
+                    animation: {
+                      enable: false,
+                      minimumValue: 0.1,
+                      speed: 40,
+                      sync: false
+                    },
+                    random: false,
+                    value: 0.5
+                  }
+                },
+                detectRetina: false,
+            }}
+        />
+      </section>
+{/* <script async="" defer="" src="https://buttons.github.io/buttons.js"></script> */}
+    </ProjectsHead>
+  )
+}
+
+const ProjectsHead = styled.header`
+width: 100%;
+height: 100vh;
+color: white;
+font-family: 'Arimo';
+
+& > img {
+    opacity: .5;
+}
+
+
+canvas {
+  display: block;
+  vertical-align: bottom;
+}
+/* ---- tsparticles container ---- */
+#tsparticles {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: 50% 50%;
+}
+
+.github {
+  bottom: 10px;
+  right: 10px;
+  position: fixed;
+  border-radius: 10px;
+  background: #fff;
+  padding: 0 12px 6px 12px;
+  border: 1px solid #000;
+}
+
+.github a:hover,
+.github a:link,
+.github a:visited,
+.github a:active {
+  color: #000;
+  text-decoration: none;
+}
+
+.github img {
+  height: 30px;
+}
+
+.github #gh-project {
+  font-size: 20px;
+  padding-left: 5px;
+  font-weight: bold;
+  vertical-align: bottom;
+}
+
+#ParticlesBackground {
+  position: fixed;
+
+  width: 100%;
+  height: 100vh;
+  /* overflow: hidden; */
+
+  & > #tsparticles {
+      overflow: hidden !important;
+  }
+}
+`;
+
+export default ProjectsHeader;
